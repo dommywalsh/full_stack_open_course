980d506a4c6ba4faa9031c71eff85b77fe24fb7d
Dominic Walsh <djcwalsh94@gmail.com>
2023-01-29 12:23:54 +0200

    Refactoring and debugging

--- part1/introduction to react/src/App.js
+++ part1/introduction to react/src/App.js
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
+
+const Display = ({counter}) => <div>{{counter}}</div>
+
+const Button = ({onClick, text}) =><button onClick={onClick}>{text}</button>
+
 const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4},
-    { name: 'Maya', age: 10},
-  ]
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
 
   return (
     <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={decreaseByOne} text='minus'/>
+      <Button onClick={setToZero} text='zero'/>
     </div>
   )
 }
