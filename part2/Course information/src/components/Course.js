12d0c421985607a3af8d1dab96e04719eec295ce
Dominic Walsh <djcwalsh94@gmail.com>
2023-02-01 14:24:15 +0200

    part 2(3) done

--- part2/Course information/src/components/Course.js
+++ part2/Course information/src/components/Course.js
@@ -4,6 +4,8 @@ const Course = ({course}) => {
   return (
   <div>
     <Header course={course} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>)
 
 }
@@ -12,12 +14,31 @@ const Header = ({course}) => {
   return (<h1>{course.name}</h1>)
 }
 
+const Part = ({name, exercises}) => {
+  return (
+    <p>{name} has {exercises} exercises</p>
+  )
+}
 
-// const Content = ({parts}) => {
-//   {parts.map((part => (
-//     <p>part.key={}</p>
-//   ))
-//   }
-// }
+
+const Content = ({parts}) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+
+    ))}
+  </>
+)
+
+const Total = ({parts}) => {
+
+  const total = parts.reduce((accumulator, part) => (
+    accumulator += part.exercises
+  ), 0);
+
+  return (
+    <p>The total number of modules is {total}</p>
+  )
+}
 
 export default Course
