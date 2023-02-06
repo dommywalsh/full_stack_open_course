6887b91895f5b6807413424be9d0ec391a3878cf
Dominic Walsh <djcwalsh94@gmail.com>
2023-02-06 12:45:29 +0200

    2.6

--- part2/practise_file/src/App.js
+++ part2/practise_file/src/App.js
@@ -3,10 +3,13 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
+  const [showAll, setShowAll] = useState(true)
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
 
   const addNote = (event) => {
     event.preventDefault()
@@ -29,6 +32,9 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+      </button>
       <ul>
         {notes.map(note =>
           <Note key={note.id} note={note} />
