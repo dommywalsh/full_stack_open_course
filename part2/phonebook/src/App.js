eadbe87e8e5da4ba9e89599216b35ba9ab0c2ac5
Dominic Walsh <djcwalsh94@gmail.com>
2023-02-07 17:35:37 +0200

    finally got the search to work

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,45 +1,79 @@
 import { useState } from "react";
+import Person from "./components/person";
 
 const App = () => {
 
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ])
-  const [newName, setNewName] = useState('james')
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setNewFilter] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState([])
+
+  const handleFilter = (event) => {
+    let filterWord = event.target.value;
+    console.log(filterWord)
+    setNewFilter(filterWord);
+    const filtered = persons.filter((person) => person.name.toLowerCase()
+      .includes(filterWord.toLowerCase()));
+
+    setFilteredPersons(filtered);
+  }
 
   const handleAddName = (event) => {
     event.preventDefault();
 
     const newNameObject = {
-      name: newName
+      name: newName,
+      number: newNumber,
+      id: (persons.length + 1)
     };
 
-    const uniqueName = persons.some((person) => person.name === newName)
+    let uniqueName = persons.some((person) => person.name === newName)
 
 
     if (!uniqueName){
-      setPersons(persons.push(newNameObject))
+      setPersons(persons.concat(newNameObject))
     } else {
-      alert('that name is already taken')
-    }
+      alert(`${newName} is already taken`)
+    };
 
+    setNewName('')
+    setNewNumber('')
     console.log(persons)
   }
-
-  const Person = ({props}) => (
-    <p>{props.name}</p>
-  )
-
-
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          search:
+          <input
+          value={filter}
+          onChange={handleFilter}
+          />
+        </div>
+        <ul>
+          {filteredPersons.map(person =>
+            <Person name={person.name} key={person.id} number={person.number}/>
+            )}
+        </ul>
+        <h2>Add a new number</h2>
       <form onSubmit={handleAddName}>
         <div>
           name:
@@ -48,12 +82,23 @@ const App = () => {
           onChange={handleNameChange}
           />
         </div>
+        <div>number:
+          <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
+        </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => (
+          <Person name={person.name} key={person.id} number={person.number}/>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ part2/phonebook/src/components/person.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Person = (props) => (
+  <li>{props.name}: {props.number}</li>
+)
+
+export default Person;
