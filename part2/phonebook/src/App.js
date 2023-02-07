666532909c177e66f75b4caeed0a947a57244ab5
Dominic Walsh <djcwalsh94@gmail.com>
2023-02-07 13:13:28 +0200

    trying to figure out what's going on with these forms

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,52 @@
 import { useState } from "react";
 
 const App = () => {
+
   const [persons, setPersons] = useState([
     {name: 'Arto Hellas'}
   ])
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('james')
+
+  const handleAddName = (event) => {
+    event.preventDefault();
+
+    const newNameObject = {
+      name: newName
+    };
+
+    const uniqueName = persons.some((person) => person.name === newName)
+
+
+    if (!uniqueName){
+      setPersons(persons.push(newNameObject))
+    } else {
+      alert('that name is already taken')
+    }
+
+    console.log(persons)
+  }
+
+  const Person = ({props}) => (
+    <p>{props.name}</p>
+  )
+
+
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddName}>
         <div>
-          name: <input/>
+          name:
+          <input
+          value={newName}
+          onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type='submit'>add</button>
