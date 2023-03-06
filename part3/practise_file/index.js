172af79fbafeaf6696393a69d6fefbfe9e968290
Dominic Walsh <djcwalsh94@gmail.com>
2023-03-06 14:33:45 +0200

    that should be everything

--- part3/practise_file/index.js
+++ part3/practise_file/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -19,6 +21,11 @@ let notes = [
   }
 ]
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+  return maxId + 1
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -28,11 +35,40 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  // define parameteres for routes in express by using the colon syntax
-  const id = request.params.id
-  console.log(request.params)
+  const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
-  response.json(note)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }
+
+  notes = notes.concat(note);
+  response.json(note);
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
