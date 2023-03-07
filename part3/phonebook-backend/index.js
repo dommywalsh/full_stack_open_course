020351ef90bbd972ae527008522c48c30bc6dfc1
Dominic Walsh <djcwalsh94@gmail.com>
2023-03-07 17:36:10 +0200

    first section of node done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.use(express.json())
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    req.method === 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ')
+}))
+app.use(express.json());
+
+const takenName = (name) => persons.some(person => person.name === name)
 
 let persons = [
   {
@@ -57,7 +70,32 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
 
+  const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  if(takenName(body.name)){
+    return response.status(400).json({
+      error: `${body.name} is already taken`
+    })
+  }
+
+ const person = {
+  "id": maxId + 1,
+  "name": body.name,
+  "number": body.number
+ }
+
+ persons = persons.concat(person);
+ response.json(person)
+})
 
 
 const PORT = 3001
